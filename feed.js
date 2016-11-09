7601fbede63dd8a90710d7df7583b944760dc548
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-11-09 13:48:28 -0200

    updated all modules

--- feed.js
+++ feed.js
@@ -1,58 +1,52 @@
-module.exports = function() {
+const harp = require('./harp.json').globals;
+const RSS = require('rss');
+const moment = require('moment');
+const fs = require('fs');
 
-  var harp = require("./harp.json")["globals"]
-    , RSS = require("rss")
-    , moment = require("moment")
-    , fs = require("fs")
-    , rootUrl = harp.root_url[process.env.NODE_ENV]
-    , imageRootUrl = rootUrl + "assets/images/"
-    , outputFeed = "public/rss.xml"
-    , videos = []
-  ;
+const rootUrl = harp.root_url.production;
+const imageRootUrl = `${rootUrl}assets/images/`;
+const outputFeed = 'www/rss.xml';
+const videos = [];
+const feed = new RSS({
+  title: harp.title,
+  description: harp.description,
+  feed_url: `${rootUrl}rss.xml`,
+  site_url: rootUrl,
+  image_url: `${imageRootUrl}${harp.slug_title}-128.png`,
+  managingEditor: harp.author,
+  pubDate: moment().format('LLLL'),
+  ttl: 60
+});
 
-  var feed = new RSS({
-    title: harp.title,
-    description: harp.description,
-    feed_url: rootUrl + "rss.xml",
-    site_url: rootUrl,
-    image_url: imageRootUrl + "devfreecasts-128.png",
-    managingEditor: harp.author,
-    pubDate: moment().format("LLLL"),
-    ttl: 60
-  });
-  
-  harp.platforms.forEach(function(platformName) {
-    var platform = require("./public/"+ platformName +"/_data.json")["index"];
-
-    platform.videos.forEach(function(video) {
-      var videoId = video.title.replace(/[^\w\s]/g, "").replace(/\s/g, "-").toLowerCase();
-      var videoLink = rootUrl + platformName + "/#" + videoId;
-      var videoPublishedAt = moment(video.published_at || moment().format("YYYYMMDD"), "YYYYMMDD");
-      var videoImage = imageRootUrl + (video.image || platform.image);
-
-      videos.push({
-        title: "Video: " + video.title,
-        description: video.description,
-        url: videoLink,
-        author: harp.author,
-        date: videoPublishedAt.format("ll"),
-        enclosure: {
-          url: videoImage
-        }
-      });
+harp.platforms.forEach((platformName) => {
+  const platform = require(`./public/${platformName}/_data.json`).index;
+  platform.videos.forEach((video) => {
+    const videoId = video.title.replace(/[^\w\s]/g, '').replace(/\s/g, '-').toLowerCase();
+    const videoLink = `${rootUrl}${platformName}/#${videoId}`;
+    const videoPublishedAt = moment(video.published_at || moment().format('YYYYMMDD'), 'YYYYMMDD');
+    const videoImage = imageRootUrl + (video.image || platform.image);
+    videos.push({
+      title: `Video: ${video.title}`,
+      description: video.description,
+      url: videoLink,
+      author: harp.author,
+      date: videoPublishedAt.format('ll'),
+      enclosure: {
+        url: videoImage
+      }
     });
   });
+});
 
-  var sortedVideos = videos.sort(function(a, b) {
-    var a = moment(a.date, "ll").toDate().getTime();
-    var b = moment(b.date, "ll").toDate().getTime();
-    if (a < b) return 1;
-    if (a > b) return -1;
-    return 0;
-  });
+const sortedVideos = videos.sort((first, second) => {
+  const a = moment(first.date, 'll').toDate().getTime();
+  const b = moment(second.date, 'll').toDate().getTime();
+  if (a < b) return 1;
+  if (a > b) return -1;
+  return 0;
+});
 
-  sortedVideos.forEach(function(video) { feed.item(video); });
+sortedVideos.forEach(video => feed.item(video));
 
-  fs.writeFileSync(outputFeed, feed.xml());
-  console.log("Generated RSS:", outputFeed);
-};
+fs.writeFileSync(outputFeed, feed.xml());
+console.log(`Generated RSS: ${outputFeed}`);
