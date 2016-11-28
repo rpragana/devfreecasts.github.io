c6cc055fa2d5dd71c8fa494d4551d1dcd55da4df
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-11-28 14:34:22 -0200

    change stylus to sass, added PWA support and build improvements

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,15 +15,6 @@ module.exports = (grunt) => {
         NODE_ENV: 'production'
       }
     },
-    // Stylus ========================================
-    stylus: {
-      main: {
-        options: { compress: true },
-        files: {
-          'public/assets/css/application.css': 'public/assets/css/_application.styl'
-        }
-      }
-    },
     // Compress ======================================
     compress: {
       main: {
@@ -36,6 +27,17 @@ module.exports = (grunt) => {
           { expand: true, flatten: true, src: ['www/assets/js/*.js'], dest: 'www/assets/js', ext: '.gz.js' },
           { expand: true, flatten: true, src: ['www/assets/css/*.css'], dest: 'www/assets/css', ext: '.gz.css' }
         ]
+      }
+    },
+    // Image Optim ===================================
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'public/assets/images',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'public/assets/images'
+        }]
       }
     },
     // Exec Feed Generator ===========================
@@ -74,6 +76,28 @@ module.exports = (grunt) => {
         }]
       }
     },
+    // SW PreCache ===================================
+    'sw-precache': {
+      options: {
+        baseDir: 'www',
+        cacheId: harp.globals.slug_title,
+        workerFileName: 'sw.js',
+        verbose: true,
+        handleFetch: true
+      },
+      default: {
+        staticFileGlobs: [
+          'index.html',
+          '404.html',
+          'manifest.json',
+          'robots.txt',
+          'manifest.appcache',
+          'favicon.ico',
+          'assets/**/*.{css,js,woff,ttf,svg,eot,gif,png,jpg,jpeg}',
+          '**/index.html'
+        ]
+      }
+    },
     // Github Pages ==================================
     'gh-pages': {
       options: {
@@ -90,14 +114,16 @@ module.exports = (grunt) => {
   grunt.loadNpmTasks('grunt-gh-pages');
   grunt.loadNpmTasks('grunt-env');
   grunt.loadNpmTasks('grunt-contrib-compress');
-  grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-appcache');
+  grunt.loadNpmTasks('grunt-sw-precache');
   grunt.loadNpmTasks('grunt-sitemaps');
   grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-newer');
 
-  grunt.registerTask('prebuild:dev', ['clean', 'env:dev', 'shell:categories', 'stylus']);
-  grunt.registerTask('prebuild:prod', ['clean', 'env:prod', 'shell:categories', 'stylus']);
-  grunt.registerTask('build:prod', ['shell:feed', 'appcache', 'compress', 'sitemaps']);
+  grunt.registerTask('prebuild:dev', ['clean', 'env:dev', 'shell:categories', 'newer:imagemin']);
+  grunt.registerTask('prebuild:prod', ['clean', 'env:prod', 'shell:categories', 'newer:imagemin']);
+  grunt.registerTask('build:prod', ['shell:feed', 'appcache', 'sw-precache', 'compress', 'sitemaps']);
   grunt.registerTask('deploy:prod', ['gh-pages', 'clean']);
 };
